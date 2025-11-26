2af670f4eb29556bff52437531ca352f735b782b
ned1nex <matvnpc@gmail.com>
2025-11-26 16:47:10 +0300

    feat: djcnndjcd

--- src/entities/product/model/products.ts
+++ src/entities/product/model/products.ts
@@ -44,8 +44,8 @@ export const products: Product[] = [
     category: 'Стиральные машины',
     price: 54990,
     inStock: true,
-    modelPath: '/models/washing-machine.glb',
-    usdzPath: '/models/washing-machine.usdz',
+    modelPath: '/models/bosh_2.glb',
+    usdzPath: '/models/bosh_2.usdz',
     posterPath: '/posters/boss.jpg',
     articleNumber: 'WAN24140OE',
     specifications: {
